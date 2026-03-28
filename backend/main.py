f3e79b775944938c01fd2d6a9205e72574c9c21c
unknown <skylinetrader99@gmail.com>
2026-03-29 04:21:09 +0700

    update cors for deployment

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
